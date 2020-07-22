ad4e25022464c60b09b25f92b02b9bef36186a9d
Mahir <mahirgupta98@gmail.com>
2020-07-22 16:11:41 +0530

    requirements file and settings file updated

--- pipedrive/settings.py
+++ pipedrive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@r%n-ena72vzj9&f1p)jo!giau7&s!9agbo(i!%5r=&df5*0-^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.234.169.189', 'ec2-18-234-169-189.compute-1.amazonaws.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
